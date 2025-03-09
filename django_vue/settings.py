1a47e147fb104f599a07a88a654e2507c58e51b0
Gene Christhopher Romuga <genechristhopherromuga@Genes-MacBook-Air-2.local>
2025-03-09 13:10:10 +0100

    Update allowed host

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z_##v9qriup+&$*+&oo=h*ds=k+ck=-05v7)5+d!un9h*zpqqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://gwydionkristoff.azurewebsites.net', 'http://localhost:8000']
+ALLOWED_HOSTS = ['https://gwydionkristoff.azurewebsites.net', 'http://localhost:8000', 'gwydionkristoff.azurewebsites.net']
 
 
 # Application definition
